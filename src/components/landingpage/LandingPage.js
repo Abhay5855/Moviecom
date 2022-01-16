8ccf3d0b3a672ac89e5340ff018ec45eec0c4c50
Abhay5855 <ap3919529@gmail.com>
2022-01-16 22:40:46 +0530

    style:signup container with background color

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -6,13 +6,21 @@
 import React from 'react'
 import './landing.css'
 import LandingFooter from './LandingFooter';
+import Signup from './Signup';
 
 function LandingPage() {
     return (
         <div>
 
-            <h2>Movicom Landing Page</h2>
+            
 
+            <Signup />
+
+           
+
+             
+
+          
             <LandingFooter />
 
             
--- src/components/landingpage/Signup.js
+++ src/components/landingpage/Signup.js
@@ -1,14 +1,15 @@
-
-
-
-import React from 'react'
+import React from "react";
+import "./signup.css";
 
 function Signup() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="signup__container">
+      <form autoComplete="off">
+        <h3>Sign In</h3>
+        
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
