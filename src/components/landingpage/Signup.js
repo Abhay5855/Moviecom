5bae143df7c99fc99e77232a4dfaab964fdb0a94
Abhay5855 <ap3919529@gmail.com>
2022-01-16 23:38:19 +0530

    fix: style of the footer and the signup container

--- src/components/landingpage/Signup.js
+++ src/components/landingpage/Signup.js
@@ -5,8 +5,29 @@ function Signup() {
   return (
     <div className="signup__container">
       <form autoComplete="off">
-        <h3>Sign In</h3>
-        
+        <h3>Sign Up</h3>
+
+        <div className="signup__info">
+        <input className="signup__input" type='email' placeholder='Enter your email' />
+        <input className="signup__input"  type='password' placeholder='Password'/>
+
+        </div>
+
+         <div className="signup__btn">
+
+         <button className="signup__btn">
+
+Sign Up
+
+</button>
+
+         </div>
+
+       
+
+        <span>Need help?</span>
+
+
       </form>
     </div>
   );
