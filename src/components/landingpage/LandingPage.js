d4156d8dad1ea28522751bbef32d36d35c49ead4
Abhay5855 <ap3919529@gmail.com>
2022-01-17 23:44:57 +0530

    refactor: delete the navbar component

--- src/components/landingpage/LandingPage.js
+++ src/components/landingpage/LandingPage.js
@@ -6,13 +6,14 @@
 import React from 'react'
 import './landing.css'
 import LandingFooter from './LandingFooter';
+import Landing_Navbar from './Landing_Navbar';
 import Signup from './Signup';
 
 function LandingPage() {
     return (
         <div>
 
-            
+            <Landing_Navbar />
 
             
 
