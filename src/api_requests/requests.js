6fa749cae1ed5b489757ac1ae44f1326d744c64c
Abhay5855 <ap3919529@gmail.com>
2022-01-18 21:26:30 +0530

    docs: creating a axios instance create with the baseURl to display all movies

--- src/api_requests/requests.js
+++ src/api_requests/requests.js
@@ -1,13 +1,15 @@
-const API_KEY = "d97b3e0257a2c71f178ef69c4ca4f27c";
+
+
+require('dotenv').config;
 
 const requests = {
-  fetchTrending: `trending/all/day?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTrending: `trending/all/day?api_key=${process.env.API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${process.env.API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=28`,
+  fetchHorrorMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=27`,
+  fetchComedyMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=35`,
+  fetchDocumentariesMovies: `/discover/movie?api_key=${process.env.API_KEY}&with_genres=99`,
 };
 
 export default requests;
