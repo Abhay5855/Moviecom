7c40c786aceabb3ae08c891661da71c4e7dad2e5
Abhay5855 <ap3919529@gmail.com>
2022-01-19 00:30:59 +0530

    creating list of navbar lists

--- src/components/dashboard/Dashboard_Navbar.js
+++ src/components/dashboard/Dashboard_Navbar.js
@@ -1,13 +1,32 @@
-
-
-import React from 'react'
+import React from "react";
+import "./dashboard.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function Dashboard_Navbar() {
-    return (
-        <nav>
-        
-        </nav>
-    )
+  return (
+    <Router>
+      <nav className="dashboard__nav">
+        <ul>
+          <li>
+            <Link to="/">Logout</Link>
+          </li>
+
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/community">Community</Link>
+          </li>
+
+          <li>
+            <Link to="/mylist">List</Link>
+          </li>
+
+        </ul>
+      </nav>
+    </Router>
+  );
 }
 
-export default Dashboard_Navbar
+export default Dashboard_Navbar;
