82d90deacb00b635ed4cf6e3e1b328f4978d9b49
Abhay5855 <ap3919529@gmail.com>
2022-01-19 22:38:56 +0530

    docs : creating a function useAuth i.e to pass the context with useContext and handling the user mounting and unmounting inside of useEffect hook and unsubscribe the user also creating a signup function in which we are setting the current user

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -2,13 +2,68 @@
 
 
 
+import { useState, useContext, useEffect } from "react";
 import authContext from "./authContext";
+import {auth} from '../firebase/firebase'
 
-function AuthProvider() {
+
+
+
+
+    // Function that takes the context
+
+    export function useAuth () {
+
+        return useContext(authContext);
+
+           
+    }
+
+function AuthProvider(props) {
+
+
+
+
+    // check for current user or not
+
+     const [currentuser , setCurrentuser]  = useState();
+
+    //  Function signup that takes email and password
+    function signup (email , password) {
+
+           return auth.createUserWithEmailAndPassword(email , password);
+
+          
+    }
+    
+
+    useEffect(() => {
+       const unsubscribe = auth.onAuthStateChanged((user) => {
+                
+               setCurrentuser(user);
+         })
+
+         return unsubscribe;
+    }, []);
+
+
+
+    //  The value which will be provided to the the provider
+
+      const value = {
+             currentuser,
+             signup,
+      }
+
+
+
+
   return  (
     <div>
 
-        <authContext.Provider >
+        <authContext.Provider value={value}>
+
+            {props.children}
 
         </authContext.Provider>
 
