ed225c61bac64b6299b22a2181df76decfcec025
Abhay5855 <ap3919529@gmail.com>
2022-01-19 21:41:24 +0530

    docs: including the routes for signup and dashboard in routes

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -6,6 +6,7 @@ import React from 'react'
 import {BrowserRouter as Router , Route} from 'react-router-dom';
 import Dashboard from '../dashboard/Dashboard';
 import Dashboard_Comments from '../dashboard/Dashboard_Comments';
+import Signup from '../authentication/Signup'
 
 
 function MainRoutes() {
@@ -15,7 +16,8 @@ function MainRoutes() {
             <Router>
 
                 <Route exact path='/community' component={Dashboard_Comments} />
-                <Route exact path='/' component={Dashboard} />
+                <Route exact path='/dashboard' component={Dashboard} />
+                <Route exact path='/signup' component={Signup} />
             </Router>
 
            
